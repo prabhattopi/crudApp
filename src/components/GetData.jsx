b0f948b18d2addc50f33362b751e772aae9368b6
Prabhat Singh <topi9864@gmail.com>
2023-06-13 12:44:05 +0000

    the file is pushed

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -41,7 +41,7 @@ const GetData = ({ items, setItems }) => {
   };
 
   return (
-    <div className="container mx-auto px-4 my-2 overflow-y-auto max-h-80vh">
+    <div className="container px-4 my-2 overflow-y-auto max-h-80vh">
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-8">
         {items?.map((item) => (
           <div key={item._id} className="bg-white shadow-md rounded-lg p-4 flex flex-col">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import Sidebar from "./Sidebar";
 
 const Home = ({ items, setItems }) => {
   return (
-    <div className="flex flex-col md:flex-row bg-sky-800">
-    <Sidebar />
-    <div className="container mx-auto px-4 my-4 md:ml-4 flex-grow">
+    <div className="flex flex-col md:flex-row">
+  <Sidebar />
+  <div className="container bg-sky-800 h-[calc(100vh-64px)] px-4 py-4 flex-grow">
+    <GetData items={items} setItems={setItems} />
+  </div>
+</div>
 
-        <GetData items={items} setItems={setItems} />
-      </div>
-    </div>
   );
 };
 
