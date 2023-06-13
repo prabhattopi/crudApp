b55d7b86dc59251b997c498b94e4c386d6dbcac3
Prabhat Singh <topi9864@gmail.com>
2023-06-13 07:41:46 +0000

    responsive

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,6 +7,7 @@ const Home = ({ items, setItems }) => {
     <div className="flex flex-col md:flex-row bg-sky-800">
     <Sidebar />
     <div className="container mx-auto px-4 my-4 md:ml-4 flex-grow">
+
         <GetData items={items} setItems={setItems} />
       </div>
     </div>
