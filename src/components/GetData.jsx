9174deeb6030267098ac14594f8e4e1a2444dba2
Prabhat Singh <topi9864@gmail.com>
2023-06-14 06:26:39 +0000

    the ui layout changes

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -41,8 +41,8 @@ const GetData = ({ items, setItems }) => {
   };
 
   return (
-    <div className="container px-4 my-2 overflow-y-auto max-h-80vh">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-8">
+    <div className="container px-4 py-4 my-2">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-8 ">
         {items?.map((item) => (
           <div key={item._id} className="bg-white shadow-md rounded-lg p-4 flex flex-col">
             <h3 className="text-lg font-bold">{item.user}</h3>
