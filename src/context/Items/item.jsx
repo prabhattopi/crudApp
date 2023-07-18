caae6a3486e62083c20ea872e02a6ea0248185d3
Prabhat Singh <topi9864@gmail.com>
2023-07-18 07:22:31 +0000

    the file is pushed

--- src/context/Items/item.jsx
+++ src/context/Items/item.jsx
@@ -12,7 +12,7 @@ const initialState = {
   description: '',
   occupation:"",
   social_links:[],
-  img:"",
+  img:"https://firebasestorage.googleapis.com/v0/b/image-gallery-8cf2b.appspot.com/o/images%2F1689583584637.webp?alt=media&token=5320ad10-a9d0-491b-8f9f-130cffdaa3b2",
   comments:[],
   scheduleTime: '',
   loading: false,
