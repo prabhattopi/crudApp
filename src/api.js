6fac2a1d35ee66b74e4337e05632326a4a65eeae
Prabhat Singh <topi9864@gmail.com>
2023-08-23 04:17:55 +0000

    the file is save

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.VITE_API_URL, // Set the base URL for your backend API
+  baseURL:import.meta.env.VITE_API_URL, // Set the base URL for your backend API
 });
 
 export default api;
