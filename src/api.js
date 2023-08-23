03a58a27227e1f352816fb904281a14d5aa51ebc
Prabhat Singh <topi9864@gmail.com>
2023-08-23 04:38:01 +0000

    the file is saved with env file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:import.meta.env.VITE_API_URL, // Set the base URL for your backend API
+  baseURL:"https://backend-mrbv.onrender.com/api", // Set the base URL for your backend API
 });
 
 export default api;
